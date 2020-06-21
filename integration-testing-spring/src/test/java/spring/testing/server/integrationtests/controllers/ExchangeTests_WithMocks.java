d2d99e301580149f271d7493c232fe49a296615b
gilzilberfeld <gil.zilberfeld@gmail.com>
2020-06-21 08:42:01 +0300

    Added ssmall mock REST test example

--- integration-testing-spring/src/test/java/spring/testing/server/integrationtests/controllers/ExchangeTests_WithMocks.java
+++ integration-testing-spring/src/test/java/spring/testing/server/integrationtests/controllers/ExchangeTests_WithMocks.java
@@ -1,31 +1,30 @@
 package spring.testing.server.integrationtests.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import javax.servlet.ServletContext;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import spring.testing.server.configuration.ExchangeControllerConfiguration_WithMocks;
+import spring.testing.server.exceptions.UnknownCurrencyException;
 import spring.testing.server.persistence.jdbc.RateRepository;
+
 
 @SpringBootTest
 @ContextConfiguration(classes= {ExchangeControllerConfiguration_WithMocks.class})
 public class ExchangeTests_WithMocks {
 	
-	@MockBean RateRepository repository;
+	@MockBean RateRepository mockRepository;
 	@Autowired WebApplicationContext wac;
 	
 	private MockMvc mockMvc;
@@ -38,6 +37,8 @@ public class ExchangeTests_WithMocks {
 
 	@Test
 	public void whenNoRatesAvailable_thenGetReturnsAnErrorCode() throws Exception {
+		when(mockRepository.findByCurrency(anyString()))
+			.thenThrow(new UnknownCurrencyException(null));
 		this.mockMvc
 		.perform(get("/rates/currency/?name=EUR"))
     	.andExpect(status().isNotFound());
